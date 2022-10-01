8a28230acb2dd5aa523312d88c3fc4a360e18a93
Jameel Ahmed <45817812+jameel-fullstackdeveloper@users.noreply.github.com>
2022-10-01 14:10:13 +0500

    dinner guests

---
+++ 18-seeing-the-world/index.js
@@ -0,0 +1,41 @@
+let places= ["Makka","Auckland","Madina","London","Dubai"];
+
+    //Print your array in its original order.
+    console.log(places[0]);
+    console.log(places[1]);
+    console.log(places[2]);
+    console.log(places[3]);
+    console.log(places[4]);
+
+    //Print your array in alphabetical order without modifying the actual list.
+    places.sort();
+    console.log(places[0]);
+    console.log(places[1]);
+    console.log(places[2]);
+    console.log(places[3]);
+    console.log(places[4]);
+
+    //Show that your array is still in its original order by printing it.
+    console.log(places[0]);
+    console.log(places[1]);
+    console.log(places[2]);
+    console.log(places[3]);
+    console.log(places[4]);
+
+    //Print your array in reverse alphabetical order without changing the order of the original list.
+    places.reverse();
+    console.log(places[0]);
+    console.log(places[1]);
+    console.log(places[2]);
+    console.log(places[3]);
+    console.log(places[4]);
+
+//Show that your array is still in its origicnal order by printing it again.
+
+//Reverse the order of your list. Print the array to show that its order has changed.
+
+//Reverse the order of your list again. Print the list to show it’s back to its original order.
+
+//Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
+
+//Sort to change your array so it’s stored in reverse alphabetical order. Print the list to show that its order has changed.
