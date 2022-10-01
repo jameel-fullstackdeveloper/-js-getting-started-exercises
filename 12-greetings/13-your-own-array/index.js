91e6ef41ecc0bd0c632c1292a4cd48f40efd219a
Jameel Ahmed <45817812+jameel-fullstackdeveloper@users.noreply.github.com>
2022-10-01 13:19:38 +0500

    guest-list

---
+++ 12-greetings/13-your-own-array/index.js
@@ -0,0 +1,9 @@
+
+let cars= ['Honda','Suzuki','Hyundai','Changan'];
+
+//printing the number 
+console.log(`I would like to own a ${cars[0]} car. `);
+console.log(`I would like to own a ${cars[1]} car. `);
+console.log(`I would like to own a ${cars[2]} car. `);
+console.log(`I would like to own a ${cars[3]} car. `);
+
---
+++ 14-guest-list/index.js
@@ -0,0 +1,9 @@
+
+let guests= ['Ali','Imran','Kamal','Amjad'];
+
+//printing the number 
+console.log(`${guests[0]}, You are invited to join dinner today. `);
+console.log(`${guests[1]}, You are invited to join dinner today. `);
+console.log(`${guests[2]}, You are invited to join dinner today. `);
+console.log(`${guests[3]}, You are invited to join dinner today. `);
+
