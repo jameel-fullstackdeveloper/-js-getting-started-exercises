9f852c0a63706d94c5cb57abe4d568eee31a9f84
Jameel Ahmed <45817812+jameel-fullstackdeveloper@users.noreply.github.com>
2022-10-01 13:05:34 +0500

    greeting

---
+++ 12-greetings/index.js
@@ -0,0 +1,9 @@
+
+let names= ['Jameel','Ahmed','Bilala','Mahmood'];
+
+//printing the number 
+console.log(`Hello ${names[0]}`);
+console.log(`Hello ${names[1]}`);
+console.log(`Hello ${names[2]}`);
+console.log(`Hello ${names[3]}`);
+
