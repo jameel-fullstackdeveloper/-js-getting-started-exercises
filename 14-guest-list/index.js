c9970010d2a57a8705f817dfc453918ed7e00a27
Jameel Ahmed <45817812+jameel-fullstackdeveloper@users.noreply.github.com>
2022-10-01 13:24:21 +0500

    fixed the bugs

--- 14-guest-list/index.js
+++ 14-guest-list/index.js
@@ -1,7 +1,7 @@
 
 let guests= ['Ali','Imran','Kamal','Amjad'];
 
-//printing the number 
+//printing the guests 
 console.log(`${guests[0]}, You are invited to join dinner today. `);
 console.log(`${guests[1]}, You are invited to join dinner today. `);
 console.log(`${guests[2]}, You are invited to join dinner today. `);
--- 15-changing-guest-list/index.js
+++ 15-changing-guest-list/index.js
@@ -1,7 +1,10 @@
 
 let guests= ['Ali','Imran','Kamal','Amjad'];
 
-//printing the number 
+//chaning
+guests[3] = "Kamran";
+
+//printing guests 
 console.log(`${guests[0]}, You are invited to join dinner today. `);
 console.log(`${guests[1]}, You are invited to join dinner today. `);
 console.log(`${guests[2]}, You are invited to join dinner today. `);
