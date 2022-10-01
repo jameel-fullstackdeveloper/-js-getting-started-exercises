f0b76251b9a2b8b6514564bfb13cb72a06385aa8
Jameel Ahmed <45817812+jameel-fullstackdeveloper@users.noreply.github.com>
2022-10-01 14:25:54 +0500

    intentional error

---
+++ 22-intentional-error/index.js
@@ -0,0 +1,14 @@
+const countries=  [
+    {id: 1,name:"Pakistan"},
+    {id: 2,name:"India"},
+    {id: 3,name:"Kuwait"},
+    {id: 4,name:"USA"},
+    {id: 5,name:"Dubai"},
+    {id: 6,name:"New Zealand"},
+    {id: 6,name:"Sri Lanka"},
+    {id: 6,name:"Russia"},
+    {id: 6,name:"Netharland"}
+]
+
+//producing error intentionaly
+console.log(countries[9].name);
