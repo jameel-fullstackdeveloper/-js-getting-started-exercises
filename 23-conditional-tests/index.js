6621ed5e25531ae043886986ea19ebb0521153d5
Jameel Ahmed <45817812+jameel-fullstackdeveloper@users.noreply.github.com>
2022-10-01 14:44:44 +0500

    conditional test

---
+++ 23-conditional-tests/index.js
@@ -0,0 +1,53 @@
+// 1
+let bike = 'honda';
+console.log("Is bike == 'honda'? I predict True.")
+console.log(bike == 'honda')
+
+// 2
+let mobile = 'iphone';
+console.log("Is mobile == 'iphone'? I predict True.")
+console.log(mobile == 'iphone')
+
+// 3
+let water = 'nestle';
+console.log("Is water == 'nestle'? I predict True.")
+console.log(water == 'nestle')
+
+// 4
+let fruit = 'apple';
+console.log("Is fruit == 'apple'? I predict True.")
+console.log(fruit == 'apple')
+
+// 5
+let veg = 'ladyfinger';
+console.log("Is veg == 'ladyfinger'? I predict True.")
+console.log(veg == 'ladyfinger')
+
+// 6
+let light = 'philipss';
+console.log("Is light == 'philips'? I predict False.")
+console.log(light == 'philips')
+
+// 7
+let feed = 'sonafeeds';
+console.log("Is feed == 'sonafeeds'? I predict False.")
+console.log(feed == 'aone')
+
+
+// 8
+let city = 'hdyerabad';
+console.log("Is city == 'hyderabad'? I predict False.")
+console.log(city == 'karachi')
+
+
+// 9
+let country = 'pak';
+console.log("Is country == 'pak'? I predict False.")
+console.log(country == 'ind')
+
+
+// 10
+let game = 'cricket';
+console.log("Is game == 'cricket'? I predict False.")
+console.log(game == 'hocky')
+
