bfe9b74b49500d0ce766dbae2e9e7e7448db0a7e
Jameel Ahmed <45817812+jameel-fullstackdeveloper@users.noreply.github.com>
2022-10-03 11:42:59 +0500

    alien color 2

---
+++ exe_26-alien-colors-2/index.js
@@ -0,0 +1,26 @@
+
+//version # 1
+let alien_color = "green";
+
+//Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+
+if(alien_color == "green") {
+    console.log("Player just earned 5 points");
+
+} else {
+    console.log("Player just earned 10 points");
+
+} 
+
+//version # 2
+let alien_color_2 = "yellow";
+
+//Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+
+if(alien_color_2 == "green") {
+    console.log("Player just earned 5 points");
+
+} else {
+    console.log("Player just earned 10 points");
+
+} 
