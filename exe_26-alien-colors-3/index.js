1d650f74c4002e559d9cabea70203b1283b72de8
Jameel Ahmed <45817812+jameel-fullstackdeveloper@users.noreply.github.com>
2022-10-03 11:46:56 +0500

    alien colors 3

---
+++ exe_26-alien-colors-3/index.js
@@ -0,0 +1,21 @@
+
+let alien_color = "green";
+
+
+if(alien_color == "green") {
+    console.log("Player just earned 5 points");
+}
+
+
+
+if(alien_color == "green") {
+    console.log("Player just earned 10 points");
+
+} 
+
+
+if(alien_color == "red") {
+    console.log("Player just earned 15 points");
+
+} 
+
