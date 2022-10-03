c6fa47a63018be750a95f3bbd943c87d8daf5cce
Jameel Ahmed <45817812+jameel-fullstackdeveloper@users.noreply.github.com>
2022-10-03 18:11:50 +0500

    pizzas

---
+++ exe_34-pizzas/index.js
@@ -0,0 +1,8 @@
+
+ const pizzas= ['Veggie','Meat','Pepperoni'];
+
+pizzas.forEach( p => {
+    console.log(` I like ${p} pizza`);
+})
+
+console.log(`I like piza very mich \n love to eat evey day \n I really love pizza`);
