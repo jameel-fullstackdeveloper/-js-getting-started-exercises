e0c088b4bbc58b22274e3d666378a436e283b5f3
Jameel Ahmed <45817812+jameel-fullstackdeveloper@users.noreply.github.com>
2022-10-04 19:57:09 +0500

    t-shirts

---
+++ exe_36-t-shirts/index.js
@@ -0,0 +1,9 @@
+
+
+ const animals= ['Dog','Hourse','Cat'];
+
+ animals.forEach( a => {
+    console.log(`${a} would make a great pet`);
+})
+
+console.log(`Any of these animals would make a great pet!`);
