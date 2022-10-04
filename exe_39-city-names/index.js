fabea26eb21445c0be69b98112b851d8a8312eec
Jameel Ahmed <45817812+jameel-fullstackdeveloper@users.noreply.github.com>
2022-10-04 20:17:02 +0500

    city names

---
+++ exe_39-city-names/index.js
@@ -0,0 +1,11 @@
+
+function city_country(city,country) {
+    
+    console.log(`${city}, ${country}`);
+};
+
+city_country("Karachi","Pakistan");
+city_country("Hyderabad","Pakistan");
+city_country("Sydney","Australia");
+
+
