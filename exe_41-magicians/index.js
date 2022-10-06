e2f85e5ba8ca31604defb76ac7d0f16afe30c4b5
Jameel Ahmed <45817812+jameel-fullstackdeveloper@users.noreply.github.com>
2022-10-06 13:53:23 +0500

    Magicians

---
+++ exe_41-magicians/index.js
@@ -0,0 +1,10 @@
+
+const magician = ['David Copperfield','Doug Henning','Ricky Jay'];
+
+function show_magicians(magician){
+    magician.forEach(m => {
+      console.log(m);
+   });
+}
+
+show_magicians(magician);
