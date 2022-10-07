13f13f4b2575e81beb3a8246bb40a31f2bcf7979
Jameel Ahmed <45817812+jameel-fullstackdeveloper@users.noreply.github.com>
2022-10-07 14:35:22 +0500

    Create index.js

---
+++ exe_01_node_vscode/index.js
@@ -0,0 +1,8 @@
+//Install Node.js and VS Code on your computer.
+
+//downlaod vs code from here
+//https://code.visualstudio.com/download
+
+
+//downlaod node server here
+//https://nodejs.org/en/download/
